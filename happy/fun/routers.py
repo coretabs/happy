e3e8e68adf5557f2fc290b0e5531f0c68a02d792
Malek salem <malek.salem.14@gmail.com>
2018-05-31 00:50:18 +0100

    .

--- happy/fun/routers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from . import views
-
-router = routers.SimpleRouter()
-router.register(r'posts', views.PostViewSet)
-router.register(r'comments', views.CommentViewSet)

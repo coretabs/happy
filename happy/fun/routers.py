533b2f16f5b7f23592f0f2c5b038657df25288d5
Malek salem <malek.salem.14@gmail.com>
2018-05-31 00:50:18 +0100

    .

--- happy/fun/routers.py
+++ happy/fun/routers.py
@@ -4,3 +4,13 @@ from . import views
 router = routers.SimpleRouter()
 router.register(r'posts', views.PostViewSet)
 router.register(r'comments', views.CommentViewSet)
+
+"""
+urls:
+api/v1/posts/
+api/v1/posts/<id>
+api/v1/posts/<id>/comments
+api/v1/comments/
+api/v1/comments/<id>
+
+"""

a04b5e09e41c88374a0d7eae9ccd45ca599f7552
Ibrahim Badwi <ib.a.badwi@gmail.com>
2018-06-05 16:05:51 +0300

    make comments, posts use custom user

--- happy/comments/models.py
+++ happy/comments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from accounts.models import CustomUser as User
 from posts.models import Post
 
 

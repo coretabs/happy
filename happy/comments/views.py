d25ee998fc0d74a53215c496048cb348919f1cc0
Fethi TChi <adm4algnw@gmail.com>
2018-07-28 13:55:58 +0100

    add a permission file to every app

--- happy/comments/views.py
+++ happy/comments/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import permissions
-from fun.permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
--- happy/posts/views.py
+++ happy/posts/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from fun.permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action

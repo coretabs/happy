754dfb69638c2a44772b1432b6cb61c5478c872b
Malek salem <malek.salem.14@gmail.com>
2018-08-07 13:43:53 +0100

    List comments with replies

--- happy/comments/serializers.py
+++ happy/comments/serializers.py
@@ -19,7 +19,7 @@ class BaseCommentSerializer(serializers.ModelSerializer):
         
 class CommentSerializer(BaseCommentSerializer):
     replies_count = serializers.SerializerMethodField()
-    # replies = serializers.SerializerMethodField()
+    replies = serializers.SerializerMethodField()
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
@@ -31,18 +31,33 @@ class CommentSerializer(BaseCommentSerializer):
 
     def get_replies_count(self, comment):
         """ get the number of replies for single comment """
-
         return Reply.objects.filter(parent=comment).count()
 
+    def get_replies(self, instance):
+        data = Reply.objects.filter(parent=instance)
+        return data.values()
 
 class ReplySerializer(BaseCommentSerializer):
-        
+    base_comment = serializers.SerializerMethodField()
     author = serializers.ReadOnlyField(source='author.username')
+    
 
     class Meta:
         model = Reply
         extra_kwargs = {'likes': {'read_only': True},
                        'dislikes': {'read_only': True},
                        'parent': {'read_only': True}}
-        fields = '__all__'
+        
+        fields =('base_comment','id','author','content','created',
+                                'modified','parent','likes_count',
+                                'dislikes_count','likes','dislikes')
+    def get_base_comment(self,instance):
+        data = Comment.objects.filter(pk = instance.parent_id)
+        return data.values()
 
+    def to_representation(self, instance):
+        data = super(ReplySerializer, self).to_representation(instance)
+        return {"Comment": data["base_comment"], "Replies":{"id":data['id'],'author':data["author"],'content':data["content"],
+                                                            'created':data["created"],'modified':data["modified"],'parent':data["parent"],
+                                                            'likes_count':data["likes_count"],'dislikes_count':data["dislikes_count"],
+                                                            'likes':data["likes"],'dislikes':data["dislikes"]}}

8ae2cd7b6abd9eb4d53382b8c61d5bbf5fd04dc4
Fethi TChi <adm4algnw@gmail.com>
2018-08-16 21:32:51 +0100

    add EMAIL CONFIGURATION

--- happy/happy/settings/prod_settings.py
+++ happy/happy/settings/prod_settings.py
@@ -245,3 +245,11 @@ REST_AUTH_SERIALIZERS = {
 REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'accounts.serializers.RegisterSerializer',
     }
+
+# EMAIL CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'coretabstchi@gmail.com'
+EMAIL_HOST_PASSWORD = 'fethitchi'

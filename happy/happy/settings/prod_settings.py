93ae8e579c956967d8bb1393f3181efaae759103
Fethi TChi <adm4algnw@gmail.com>
2018-09-17 14:16:24 +0100

    add AWS_DEFAULT_ACL variable

--- happy/happy/settings/prod_settings.py
+++ happy/happy/settings/prod_settings.py
@@ -156,6 +156,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'

ea08a19104d7801b99f2a9640c7acf1ed7cf69bc
Fethi TChi <adm4algnw@gmail.com>
2018-09-17 15:04:47 +0100

    add AWS_QUERYSTRING_AUTH

--- happy/happy/settings/prod_settings.py
+++ happy/happy/settings/prod_settings.py
@@ -157,6 +157,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL')
+AWS_QUERYSTRING_AUTH = False
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'

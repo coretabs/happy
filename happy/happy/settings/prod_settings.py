4b628c759a0e21595967ff81edc5f3b9326dc504
Fethi TChi <adm4algnw@gmail.com>
2018-09-21 20:43:55 +0100

    fix rest framework default auth classes

--- happy/happy/settings/prod_settings.py
+++ happy/happy/settings/prod_settings.py
@@ -172,8 +172,8 @@ MEDIA_URL = '/media/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',

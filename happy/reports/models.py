7d127f52eca4b22402a9a5a977737223f6c35455
Fethi TChi <adm4algnw@gmail.com>
2018-11-14 23:00:05 +0100

    fix typo

--- happy/reports/models.py
+++ happy/reports/models.py
@@ -7,8 +7,8 @@ from posts.models import Post
 class PostReport(models.Model):
 
     REASON = (
-            ('SPAM','spam'),
-            ('VIOLENCE', 'violence'),
+            ('SPAM','Spam'),
+            ('VIOLENCE', 'Violence Content'),
         )
     reason = models.CharField(
             max_length=255,
@@ -17,7 +17,6 @@ class PostReport(models.Model):
         )
     created = models.DateTimeField(auto_now_add=True)
     reporter = models.ForeignKey(User, on_delete=models.CASCADE,related_name="reports")
-    #reason = models.TextField(max_length=256, blank=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="reports")
 
 
@@ -25,4 +24,4 @@ class PostReport(models.Model):
         ordering =["-created"]
     
     def __str__(self):
-        return f'the post with id:{self.post.id} is reported'
+        return f'post:{self.post.id} reported by: {self.reporter.username}'

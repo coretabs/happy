42424f4acdd0c1153805b4b6293cbfdc25b0fda4
Fethi TChi <adm4algnw@gmail.com>
2019-01-19 16:06:24 +0100

    fix typos

--- happy/contact/serializers.py
+++ happy/contact/serializers.py
@@ -8,8 +8,8 @@ class ContactSerializer(serializers.Serializer):
     mail_body = serializers.CharField(required=True)
 
     def save(self):
-        name = self.validated_data['name']
-        message = self.validated_data['body']
+        name = self.validated_data['sender_name']
+        message = self.validated_data['mail_body']
         email_from = settings.EMAIL_HOST_USER
         recipient_list = ['grinn.contactus@gmail.com',]
         send_mail( name, message, email_from, recipient_list )
